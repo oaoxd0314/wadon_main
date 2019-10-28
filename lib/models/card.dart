78466cff2b02d667d26ff648629bf39a39981b2c
彥維 王 <oaoxd0314@gmail.com>
2019-10-28 22:11:09 +0800

    媽 我抓的到直啦幹(待解決問題:資料傳遞方式、散佈式架構)

---
+++ lib/models/card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Card extends StatefulWidget {
+  @override
+  _CardState createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+      
+    );
+  }
+}
--- lib/page/SubscriptionPage.dart
+++ lib/page/SubscriptionPage.dart
@@ -8,34 +8,48 @@ class SubscriptionPage extends StatefulWidget {
 }
 
 class _SubscriptionPageState extends State<SubscriptionPage> {
+  getdata() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          ExpansionTile(
-            title: Text('全部社團'),
-            initiallyExpanded: true,
-            children: <Widget>[
-              ListTile(
-                title: const Text('典雅國樂社'),
-                onTap: () {
-                  {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => Clubpage(),
+        body: StreamBuilder(
+      stream: Firestore.instance.collection('club').snapshots(),
+      builder: (context, snap) {
+        if (snap.hasData) {
+          List<DocumentSnapshot> dsList = snap.data.documents;
+          return ListView.builder(
+              itemCount: dsList.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot doc = dsList[index];
+                return Container(
+                  child: ExpansionTile(
+                    title: Text('全部社團'),
+                    initiallyExpanded: true,
+                    children: <Widget>[
+                      ListTile(
+                        title: Text(doc['c_name']),
+                        subtitle: Text(doc['c_type']),
+                        onTap: () {
+                          {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => Clubpage(),
+                              ),
+                            );
+                          }
+                          setState(() {});
+                        },
+                        leading: CircleAvatar(child: Text(doc['c_leading'])),
                       ),
-                    );
-                  }
-                  setState(() {});
-                },
-              ),
-            ],
-            leading: CircleAvatar(child: Text("全")),
-          ),
-        ],
-      ),
-    );
+                    ],
+                    leading: CircleAvatar(child: Text("全")),
+                  ),
+                );
+              });
+        }
+      },
+    ));
   }
 }
